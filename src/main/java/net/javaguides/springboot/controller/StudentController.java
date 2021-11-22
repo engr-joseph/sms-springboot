0c4944d95a97c3ee359a98db06af3b8671be1dcd
EeveeAshDetective <esabellemesquita30@yahoo.com>
2021-11-22 18:02:21 +0800

    added course feature

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -59,6 +59,7 @@ public class StudentController {
 		existingStudent.setFirstName(student.getFirstName());
 		existingStudent.setLastName(student.getLastName());
 		existingStudent.setEmail(student.getEmail());
+		existingStudent.setCourse(student.getCourse());
 		
 		//save updated student object
 		studentService.updateStudent(existingStudent);
